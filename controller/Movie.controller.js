d593507846453ae2cf1d43a7f576375a68e107d0
mayyadah <miss.mayadah1@gmail.com>
2021-07-03 17:49:40 +0300

    lab10

--- controller/Movie.controller.js
+++ controller/Movie.controller.js
@@ -1,20 +1,33 @@
 'use strict';
 const axios=require('axios');
 const Movie=require('../models/Movie.model');
+const Cache=require('../utils/cache');
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
+let cache=new Cache();
+cache['data']=[];
+cache['timesmap']=Date.now();
 
 let MovieController=(req,res) => {
-    let cityName=req.query.city
+
+    let cityName=req.query.cityName
+    let movies=[];
+
+    if(cityName ){
+      if (cache.data.length > 0 ){
+        movies=cache.data.map(element => new Movie (element));
+        res.send(movies);
+    }else{
+     
     let movieURL=`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`
-    
-    let movieRes=axios.get(movieURL).then(response => {
-      let movies=response.data.results.map(element=>{
-        return new Movie (element);
-       });
-      res.json(movies)
-    }).catch(err=>{
-      res.status(500).send(`error in getting data ==> ${err}`)
-    })
+    axios.get(movieURL).then(response => {
+       movies=response.data.results.map(element => new Movie (element));
+       
+       cache['data']= movies ;
+       res.send(movies)
+
+    }).catch(error=>res.send({message:error}));
+    }}
+  
 }
 module.exports=MovieController
